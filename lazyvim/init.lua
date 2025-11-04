8a617723f78120e419d40b37c0c0152b51527e39
bruteforceboy <ogbonnachibuoyim12@gmail.com>
2025-11-04 21:26:22 +0100

    update tings

--- lazyvim/init.lua
+++ lazyvim/init.lua
@@ -9,6 +9,7 @@ vim.opt.shiftwidth = 4 -- size for autoindent
 vim.opt.tabstop = 4 -- number of spaces a <Tab> counts for
 vim.opt.softtabstop = 4 -- editing: how many spaces a Tab inserts/erases
 vim.opt.smarttab = true
+vim.o.background = "dark"
 
 -- Run ClangFormat on save for common C/C++ extensions
 vim.api.nvim_create_autocmd("BufWritePre", {
@@ -18,13 +19,54 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end,
 })
 
--- Filetype-specific mapping for C++: write and compile current file
+-- Filetype-specific mapping for C++: F5 = compile, F6 = run
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "cpp",
   callback = function()
+    local file_dir = vim.fn.expand("%:p:h")
+    local file_name = vim.fn.expand("%:t")
+    local base_noext = vim.fn.expand("%:t:r")
+
+    -- F5: write and compile
     vim.keymap.set("n", "<F5>", function()
       vim.cmd("write")
-      vim.cmd("belowright split | terminal g++ -std=c++23 -DLOCAL % -o %:r")
+      -- Use bash -c and single quotes to avoid fish parsing issues
+      vim.cmd(
+        "belowright split | terminal bash -c 'cd \""
+          .. file_dir
+          .. '" && g++ -std=c++23 -DLOCAL "'
+          .. file_name
+          .. '" -o "'
+          .. base_noext
+          .. '" && echo "[g++] exit:$?" ; exec $SHELL\''
+      )
+    end, { buffer = true, noremap = true, silent = true })
+
+    -- F6: write and run compiled binary
+    vim.keymap.set("n", "<F6>", function()
+      vim.cmd("write")
+      vim.cmd(
+        "belowright split | terminal bash -c 'cd \""
+          .. file_dir
+          .. '" && if [ -x "'
+          .. base_noext
+          .. '" ]; then ./'
+          .. base_noext
+          .. '; else echo "Executable not found: '
+          .. base_noext
+          .. "\"; fi; exec $SHELL'"
+      )
     end, { buffer = true, noremap = true, silent = true })
   end,
+})
+
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = "●", -- a small marker before the message (optional)
+    spacing = 2,
+  },
+  signs = true, -- show icons in the sign column
+  underline = true, -- underline problem ranges
+  update_in_insert = false, -- don't spam while inserting
+  severity_sort = true,
 })
---
+++ lazyvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  -- palenight port (pick one you like)
+  { "alexmozaidze/palenight.nvim", priority = 1000 },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "palenight", -- plugin exposes "palenight" name
+    },
+  },
+}
