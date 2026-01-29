9ef5fd941273d83ccf9d2ddf06fd590e944d2f5c
bruteforceboy <ogbonnachibuoyim12@gmail.com>
2026-01-29 18:18:03 +0100

    add lazgit plugin (from lazygit)

---
+++ lazyvim/lua/plugins/lazygit.lua
@@ -0,0 +1,16 @@
+-- lua/plugins/lazygit.lua (for lazy.nvim)
+return {
+  "kdheepak/lazygit.nvim",
+  lazy = true,
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+  },
+}
